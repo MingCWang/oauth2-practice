98561e3aa6e1f17ebdbe11bcdae7f7705d651847
Ming-Shih <minstone@gmail.com>
2023-07-09 23:49:01 +0700

    implement logout

--- frontend/src/components/HomeSideBar.jsx
+++ frontend/src/components/HomeSideBar.jsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom';
 
+
+function Logout() {
+	localStorage.removeItem('jwt');
+	window.location.reload();
+}
 // eslint-disable-next-line react/prop-types
 export default function HomeSideBar({ user }) {
 
@@ -11,11 +16,17 @@ export default function HomeSideBar({ user }) {
 				<nav>
 					<ul id="navbar">
 						<Link to={`/`}>Home</Link>
-						{user == '' && (<Link to={`/login`}>Login</Link>)}
-
+						{user == '' && (
+							<Link to={`/login`}>Login</Link>
+						)}
+						{user != '' && (
+							<Link onClick={() => Logout()}>Logout</Link>
+						)}
 						<Link to={`/about`}>About</Link>
 					</ul>
-					{user != '' && (<div id="user">{user}</div>)}
+					{user != '' && (
+						<div id="user">{user}</div>
+					)}
 				</nav>
 				<h1 id="title">OAuth2.0 practice</h1>
 
